0a1d6b1f581418bcc4f97dc4add9de809e2c3a57
Kancharla Nithyasri <kancharlanithyasri731@gmail.com>
2025-07-30 15:24:59 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -8,5 +8,5 @@ def hello():
     return  "HELLO WORLD"
 
 if __name__=="__main__":
-    port=int(os.envron.get("PORT",5000))
+    port=int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
